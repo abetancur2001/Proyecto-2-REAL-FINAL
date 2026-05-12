4e5ee587e0dc314f57d2a9e6b9ec773c86096d8d
abetancur2001 <a.betancurc@uniandes.edu.co>
2026-05-11 21:48:12 -0500

    inicio tests

---
+++ BoardGameCafe/src/tests/CaféTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import modelo.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CaféTest {
+	
+	private Café cafe;
+	
+	public void setUp() {
+		
+		
+		
+	}
+
+}
